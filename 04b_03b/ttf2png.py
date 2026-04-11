661815a0d4c905a935231a77e792c6224f256907
Gary Benson <gary@gbenson.net>
2026-04-11 13:27:23 +0100

    Render 04B_03B_.TTF as 04b_03b.png

---
+++ 04b_03b/ttf2png.py
@@ -0,0 +1,23 @@
+from zipfile import ZipFile
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def main():
+    with ZipFile("04b_03b.zip") as zf:
+        with zf.open("04B_03B_.TTF") as fp:
+            font = ImageFont.truetype(fp, size=8)
+
+    img = Image.new("1", (16*8, 6*8), 255)
+    d = ImageDraw.Draw(img)
+
+    for o in range(33, 127):
+        x = (o & 15) << 3
+        y = ((o & 0xf0) >> 1) - 16
+        d.text((x, y), chr(o), fill=0, font=font)
+
+    img.save("04b_03b.png")
+
+
+if __name__ == "__main__":
+    main()
